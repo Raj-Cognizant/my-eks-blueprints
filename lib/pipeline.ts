6ac01f716d73d41aa4ac67a7d64c00b1de4403e5
EC2 Default User <ec2-user@ip-172-31-33-64.us-west-2.compute.internal>
2022-06-21 16:40:32 +0000

    Setting up EKS Blueprints deployment pipeline

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -17,9 +17,9 @@ export default class PipelineConstruct extends Construct {
   
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-workshop-pipeline")
-      .owner("your-github-username")
+      .owner("Raj-Cognizant")
       .repository({
-          repoUrl: 'your-repo-name',
+          repoUrl: 'my-eks-blueprints',
           credentialsSecretName: 'github-token',
           targetRevision: 'main'
       })
